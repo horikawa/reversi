def5191477ab1c25e75d0f9045660e970ab0ad46
horikawa <joho@bemax.jp>
2011-12-14 15:49:04 +0900

    0.0.2

--- main.rb
+++ main.rb
@@ -1,28 +1,88 @@
 require 'sdl'
 
-WIDTH = 640
-HEIGHT = 400
+WIDTH = 800
+HEIGHT = 500
+BOARD_LEFT= 50
+BOARD_TOP=50
+MENU_LEFT=500
+MENU_TOP=50
 
 SDL.init(SDL::INIT_EVERYTHING)
 screen = SDL.set_video_mode(WIDTH,HEIGHT,0,SDL::SWSURFACE)
 
+white = 2
+black = 2
+player = 1
+board = [
+	[0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0],
+	[0,0,0,2,1,0,0,0],
+	[0,0,0,1,2,0,0,0],
+	[0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0]
+]
+
 loop do
 	t1 = Time.now
 
-	#イベント処理
+	#event_settings
 	while event = SDL::Event2.poll do
 		case event
 			when SDL::Event2::Quit
 				exit
+			when SDL::Event2::MouseButtonDown
+			when SDL::Event2::MouseMotion
 		end
 	end
 
-	screen.fill_rect(0,0,WIDTH,HEIGHT,[0,255,0]) #矩形塗りつぶし
+	#draw screen
+	screen.lock
 
-puts "aaa"
+	#screen_initialize
+	screen.fill_rect 0,0,WIDTH,HEIGHT,[100,100,100] #矩形塗りつぶし
+	
+	#board_initialize
+	screen.fill_rect BOARD_LEFT, BOARD_TOP, 400, 400, [0,128,0]
+	9.times do |idx|
+		x0 = BOARD_LEFT + 50 * idx
+		y0 = BOARD_TOP
+		screen.draw_line x0, y0, x0, y0 + 400, [0,0,0]
+	end
+	9.times do |idx|
+		x0 = BOARD_LEFT
+		y0 = BOARD_TOP + 50 * idx
+		screen.draw_line x0, y0, x0 + 400, y0, [0,0,0]
+	end	
+	
+	#board_show
+	board.each_with_index do |line,row|
+		line.each_with_index do |elm,col|
+			if elm!=0 then
+				x = BOARD_LEFT + row * 50 + 25
+				y = BOARD_TOP + col * 50 + 25
+				if elm==1 then
+					screen.draw_circle x+1,y+1,20,[255,255,255],true
+					screen.draw_circle x,y,20,[0,0,0],true
+				else
+					screen.draw_circle x+1,y+1,20,[0,0,0],true
+					screen.draw_circle x,y,20,[255,255,255],true
+				end
+			end
+		end
+	end
 
-	screen.update_rect(0,0,0,0) #画面更新
+	screen.draw_circle MENU_LEFT+25,MENU_TOP+25,20,[0,0,0],true
+	screen.draw_circle MENU_LEFT+25,MENU_TOP+75,20,[255,255,255],true
 
+	#screen unlock
+	screen.unlock
+
+	#screen update
+	screen.update_rect(0,0,0,0)
+	
+	#interval managemant
 	t2 = Time.now
 	diff = t2 - t1
 	if diff < 0.015
